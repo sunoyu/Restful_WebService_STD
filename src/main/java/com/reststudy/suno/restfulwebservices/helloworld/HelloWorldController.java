b506c93493c7da1f411c310544aab43e067382cc
sunoyu <suno_yu@naver.com>
2023-12-23 14:27:23 +0900

    User Dao

---
+++ src/main/java/com/reststudy/suno/restfulwebservices/helloworld/HelloWorldController.java
@@ -0,0 +1,28 @@
+package com.reststudy.suno.restfulwebservices.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping(path = "/hello-world")
+    public String helloworld() {
+        return "Hello world";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
+    }
+
+    // Path Parameters
+    // /users/{id}/todos/{id}  => /users/2/todos/200
+    // /hello-world/path-variable/{name}
+    // /hello-world/path-variable/Ranga
+
+    @GetMapping(path = "/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+}
---
+++ src/test/java/com/reststudy/suno/restfulwebservices/RestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reststudy.suno.restfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
