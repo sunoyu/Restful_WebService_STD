66808c6a438d90a1db60f960b58fcdf987358182
sunoyu <suno_yu@naver.com>
2023-12-23 14:45:52 +0900

    1. User 컨트롤러 추가 2. 서비스 기능 구현(findAll, findOue)

--- src/main/java/com/reststudy/suno/restfulwebservices/user/UserDaoService.java
+++ src/main/java/com/reststudy/suno/restfulwebservices/user/UserDaoService.java
@@ -3,6 +3,7 @@ package com.reststudy.suno.restfulwebservices.user;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 import org.springframework.stereotype.Component;
 
@@ -25,6 +26,9 @@ public class UserDaoService {
 
     //public User save(User user) {
 
-    //public User findOne(int id) {
+    public User findOne(int id) {
+        Predicate<? super User> predicate = user -> user.getId().equals(id);
+        return users.stream().filter(predicate).findFirst().get();
+    }
 
 }
---
+++ src/main/java/com/reststudy/suno/restfulwebservices/user/userResource.java
@@ -0,0 +1,27 @@
+package com.reststudy.suno.restfulwebservices.user;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class userResource {
+    private UserDaoService service;  // 오토와이어드 필요 없이 아래 ⬇️⬇️ 생성자에 주입해주면 된다.
+
+    public userResource(UserDaoService service) {
+        this.service = service;
+    }
+
+    @GetMapping(path = "/users")
+    public List<User> retrieveAllUsers() {
+        return service.findAll();
+    }
+
+    @GetMapping(path = "users/{id}")
+    public User retrieveUser(@PathVariable int id) {
+        return service.findOne(id);
+    }
+
+}
