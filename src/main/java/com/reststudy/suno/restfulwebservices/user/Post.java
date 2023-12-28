e8435340609176567a2e9771714883c7512a1677
sunoyu <suno_yu@naver.com>
2023-12-28 23:35:10 +0900

    30단계: User엔티티와 일대다 관계로 Post 엔티티 생성하기

---
+++ src/main/java/com/reststudy/suno/restfulwebservices/user/Post.java
@@ -0,0 +1,34 @@
+package com.reststudy.suno.restfulwebservices.user;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+// 게시글을 생성, 조회하는 객체
+@Entity
+public class Post {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)      // 다대일, 게시글과 유저의 관계를 보면 유저가 많은 게시를 관리하니깐.
+    @JsonIgnore
+    private User user;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- src/main/java/com/reststudy/suno/restfulwebservices/user/User.java
+++ src/main/java/com/reststudy/suno/restfulwebservices/user/User.java
@@ -1,20 +1,23 @@
 package com.reststudy.suno.restfulwebservices.user;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Entity(name = "user_details")
 public class User {
     protected User(){};   // 기본 생성자를 생성해주지 않으면 JPA에서 에러가 발생함(웹과 DB연결 시 에러 발생)
 
     @Id
-    @GeneratedValue
+    @GeneratedValue     // db테이블 상 시퀀스열이 생김
     private Integer id;
     @Size(min = 2, message = "이름은 최소 2자 이상으로 입력하세요.")
 //    @JsonProperty("user_name")
@@ -22,6 +25,10 @@ public class User {
     @Past(message = "과거 날짜로 입력하세요.")
 //    @JsonProperty("birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(mappedBy = "user")    // 1대다, user에 의해 관리된다.
+    @JsonIgnore
+    private List<Post> posts;
 
     public User(Integer id, String name, LocalDate birthDate) {
         super();
